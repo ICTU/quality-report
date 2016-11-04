a7d7a22989fd751e32b4a62611db90c7d3000730
Frank Niessink <frank@niessink.com>
2016-11-04 14:51:46 +0100

    Add integration tests.

---
+++ python/integrationtests/integration_tests.py
@@ -0,0 +1,40 @@
+"""
+Copyright 2012-2016 Ministerie van Sociale Zaken en Werkgelegenheid
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+import unittest
+import os
+import tempfile
+import bs4
+
+
+class IntegrationTests(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        """ Create the report. """
+        cls.report_folder = tempfile.mkdtemp()
+        os.system('python quality_report.py --project integrationtests/test_all_requirements_no_sources '
+                  '--report {} --log ERROR'.format(cls.report_folder))
+
+    def test_report_exists(self):
+        """ Test that the report exists. """
+        self.failUnless(os.path.exists('{}/index.html'.format(self.report_folder)))
+
+    def test_report_title(self):
+        """ Test the report title. """
+        with open('{}/index.html'.format(self.report_folder)) as contents:
+            soup = bs4.BeautifulSoup(contents.read(), "html.parser")
+            self.assertEqual('Kwaliteitsrapportage ICTU/Quality report with all requirements but no sources',
+                             soup('head')[0]('title')[0].string)
---
+++ python/integrationtests/test_all_requirements_no_sources/project_definition.py
@@ -0,0 +1,90 @@
+""" Project definition for testing a project with all requirements but no sources. """
+
+from qualitylib.domain import Project, Product, Team, Document, Person, Application, Component
+from qualitylib import metric_source, requirement
+
+from isd.convention import HISTORY_FILE_LOCATION
+
+
+# Locations, URLs and addresses of back end tools and systems
+
+HISTORY_LOC = HISTORY_FILE_LOCATION('test_all_requirements_no_sources')
+
+# Sources
+HISTORY = metric_source.History(HISTORY_LOC)
+
+# The project
+PROJECT = Project(
+    'ICTU', name='Quality report with all requirements but no sources',
+    metric_sources={metric_source.History: HISTORY},
+    requirements=[requirement.TrustedProductMaintainability, requirement.Web, requirement.JavaScript,
+                  requirement.CSharp, requirement.Java, requirement.TrackManualLTCs,
+                  requirement.TrackBugs, requirement.TrackTechnicalDebt,
+                  requirement.TrackActions, requirement.TrackRisks, requirement.TrackSecurityAndPerformanceRisks,
+                  requirement.TrackReadyUS, requirement.TrackJavaConsistency,
+                  requirement.TrackCIJobs, requirement.TrackSonarVersion])
+
+# Documents.
+DOC = Document(name='Document XYZ')
+PROJECT.add_document(DOC)
+
+# Teams of the project.
+TEAM = Team(
+    name='Team 123',
+    requirements=[requirement.ScrumTeam, requirement.TrackSpirit, requirement.TrackAbsence])
+TEAM.add_member(Person(name='Person 1'))
+TEAM.add_member(Person(name='Person 2'))
+PROJECT.add_team(TEAM)
+
+# Products the project(s) develop(s).
+PRODUCT = Product(
+    PROJECT, 'PR', name='Product ABC',
+    requirements=[requirement.OWASPDependencies, requirement.OWASPZAP, requirement.OpenVAS,
+                  requirement.UserStoriesAndLTCs, requirement.UnitTests, requirement.ART, requirement.ARTCoverage,
+                  requirement.CodeQuality, requirement.Performance,
+                  requirement.NoSnapshotDependencies, requirement.TrackBranches],
+    unittests=Product(
+        PROJECT, name='Product ABC unit tests'),
+    art=Product(
+        PROJECT, name='Product ABC ART', requirements=[requirement.CodeQuality, requirement.TrackBranches]),
+    jsf=Product(
+        PROJECT, name='Product ABC JSF', requirements=[requirement.JSFCodeQuality]))
+
+PRODUCT_V1 = Product(
+    PROJECT, 'PT', name='Product ABC v1',
+    product_version='1',
+    requirements=[requirement.OWASPDependencies, requirement.OWASPZAP, requirement.UserStoriesAndLTCs,
+                  requirement.UnitTests, requirement.ART, requirement.CodeQuality, requirement.Performance,
+                  requirement.NoSnapshotDependencies, requirement.TrackBranches],
+    unittests=Product(
+        PROJECT, name='Product ABC v1 unit tests'),
+    art=Product(
+        PROJECT, name='Product ABC v1 ART', requirements=[requirement.CodeQuality, requirement.TrackBranches]),
+    jsf=Product(
+        PROJECT, name='Product ABC v1 JSF', requirements=[requirement.JSFCodeQuality]))
+
+APPLICATION = Application(PROJECT, 'AP', name='Application FOO')
+
+COMPONENT = Component(PROJECT, 'CO', name='Component FOO')
+
+PROJECT.add_product(PRODUCT)
+PROJECT.add_product(PRODUCT_V1)
+PROJECT.add_product(APPLICATION)
+PROJECT.add_product(COMPONENT)
+
+# Dashboard layout
+
+# Columns in the dashboard is specified as a list of tuples. Each tuple
+# contains a column header and the column span.
+DASHBOARD_COLUMNS = [('Products', 1), ('Teams', 1), ('Algemeen', 2)]
+
+# Rows in the dashboard is a list of row tuples. Each row tuple consists of
+# tuples that describe a cell in the dashboard. Each cell is a tuple containing
+# the product or team and the color. Optionally the cell tuple can contain a
+# third value which is a tuple containing the column and row span for the cell.
+DASHBOARD_ROWS = [((PRODUCT, 'lightsteelblue'), (TEAM, 'lavender', (1, 4)), ('PD', 'lightgrey'), ('PE', 'lightgrey')),
+                  ((PRODUCT_V1, 'lightsteelblue'), ('PC', 'lightgrey', (1, 3)), ('MM', 'lightgrey', (1, 3))),
+                  ((APPLICATION, 'lightsteelblue'),),
+                  ((COMPONENT, 'lightsteelblue'),)]
+
+PROJECT.set_dashboard(DASHBOARD_COLUMNS, DASHBOARD_ROWS)
