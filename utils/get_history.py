262e99917ed409a89e8cd5172c7f027282d175a4
Frank Niessink <frank@niessink.com>
2016-12-18 12:10:04 +0100

    Estimate time remaining. Use subprocess module instead of os.system.

--- utils/get_history.py
+++ utils/get_history.py
@@ -15,20 +15,23 @@ limitations under the License.
 """
 
 # Utility to recreate a complete history file from the committed revisions.
-# This script assumes Subversion was used to store the history file.
-# The strategy is to get the last line of each revision and append it to the
-# complete history file. Three classes do the work:
-# - LastRevisionProcessed is used to get and set the last revision processed, so we can
-#   continue where we left off after a restart.
+#
+# This script assumes Subversion was used to store the history file. The strategy is to get the last line of each
+#  revision and append it to the complete history file. Four classes do the work:
+# - TimeEstimator estimates how more time is needed to collect the revisions still to be collected based on the time
+#   it took to collect the revisions so far.
+# - LastRevisionProcessed is used to get and set the last revision processed, so we can continue where we left off
+#   after a restart.
 # - RevisionsToCollect is a list of the revisions still to be collected, retrieved using svn log.
-# - RevisionCollector gets the revisions from Subversion one by one and adds the
-#   last line of each revision to the full history file.
+# - RevisionCollector gets the revisions from Subversion one by one and adds the last line of each revision to the
+#   full history file.
 
 
 import argparse
+import datetime
 import logging
 import os
-import sys
+import subprocess
 import xml.etree.ElementTree
 
 
@@ -66,18 +69,31 @@ class LastRevisionCollected(object):
             last_revision_file.write(bytes(revision))
 
 
+class TimeEstimator(object):
+    def __init__(self, total_steps):
+        super(TimeEstimator, self).__init__()
+        self.__total_steps = total_steps
+        self.__start_time = datetime.datetime.now()
+
+    def time_remaining(self, steps_done):
+        """ Return the estimated amount of time needed to do the work. """
+        if steps_done:
+            seconds_per_step = (datetime.datetime.now() - self.__start_time).total_seconds() / steps_done
+            seconds_remaining = (self.__total_steps - steps_done) * seconds_per_step
+            return str(datetime.timedelta(seconds=round(seconds_remaining)))
+        return 'not enough data to estimate'
+
+
 class RevisionsToCollect(list):
     """ The revisions of the history file still to collect. """
 
     def __init__(self, url, last_revision):
-        filename = 'history.json.log.xml'
         start_revision = last_revision.get() + 1 if last_revision.get() else 0
-        logging.info('svn log --xml -r %d:HEAD %s > %s', start_revision, url, filename)
-        os.system('svn log --xml -r {}:HEAD {} > {}'.format(start_revision, url, filename))
-        tree = xml.etree.ElementTree.parse(filename)
-        revisions = sorted([int(log_entry.attrib['revision']) for log_entry in tree.getroot()])
-        logging.info('Read %d revisions from %s', len(revisions), filename)
-        super(RevisionsToCollect, self).__init__(revisions)
+        logging.info('svn log --xml -r %d:HEAD %s', start_revision, url)
+        revisions_xml = subprocess.check_output(['svn', 'log', '--xml', '-r', '{}:HEAD'.format(start_revision), url])
+        root = xml.etree.ElementTree.fromstring(revisions_xml)
+        super(RevisionsToCollect, self).__init__(sorted([int(log_entry.attrib['revision']) for log_entry in root]))
+        logging.info('%d revisions to collect from %s', len(self), url)
 
 
 class RevisionCollector(object):
@@ -91,15 +107,29 @@ class RevisionCollector(object):
     def collect(self, revisions):
         """ Get the revisions and append the last measurement of each revision to the full history file. """
         nr_revisions = len(revisions)
-        for index, revision in enumerate(revisions):
-            logging.info('Retrieving revision %s (%s/%s)', revision, index + 1, nr_revisions)
-            self.__get_revision(revision)
-            self.__last_revision.set(revision)
+        estimate = TimeEstimator(nr_revisions)
+        for index, revision_number in enumerate(revisions):
+            last_measurement = self.__get_last_measurement(revision_number)
+            self.__write_measurement(last_measurement)
+            self.__last_revision.set(revision_number)
+            logging.info('Revision: %s, %s/%s, measurement date: %s, time remaining: %s', revision_number, index + 1,
+                         nr_revisions, self.__get_date(last_measurement), estimate.time_remaining(index))
 
-    def __get_revision(self, revision):
-        """ Get the revision and append the measurement to the full history file. """
-        if os.system('svn cat -r {} {} | tail -n 1 >> {}'.format(revision, self.__url, self.__filename)):
-            sys.exit('Subversion terminated abnormally')
+    def __get_last_measurement(self, revision_number):
+        """ Get the revision and extract the last measurement (the measurement that was new to this revision). """
+        revision = subprocess.check_output(['svn', 'cat', '-r', str(revision_number), self.__url],
+                                           universal_newlines=True).strip()
+        return revision.rsplit('\n', 1)[-1] if revision else ''
+
+    def __write_measurement(self, measurement):
+        """ Append the measurement to the history file. """
+        with open(self.__filename, mode='a') as history_file:
+            history_file.write(measurement + '\n')
+
+    @staticmethod
+    def __get_date(measurement):
+        """ Get the date of the measurement from the JSON. """
+        return eval(measurement).get('date') if measurement else 'unknown date'
 
 
 if __name__ == '__main__':
