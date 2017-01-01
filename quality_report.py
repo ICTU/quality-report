db5a519e7912c59e8841770a5dda08910dfae09d
Frank Niessink <frank@niessink.com>
2017-01-01 02:09:19 +0100

    Update copyright notice.

--- quality_report.py
+++ quality_report.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 """
-Copyright 2012-2016 Ministerie van Sociale Zaken en Werkgelegenheid
+Copyright 2012-2017 Ministerie van Sociale Zaken en Werkgelegenheid
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
