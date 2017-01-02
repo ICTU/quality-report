ce9c153d04822c137ca4a6a18ea1c7f2aae575bf
Frank Niessink <frank@niessink.com>
2017-01-02 19:57:22 +0100

    Suppress PyLint warnings.

--- hqlib/metric/product/unittest_metrics.py
+++ hqlib/metric/product/unittest_metrics.py
@@ -20,7 +20,7 @@ from ... import metric_info
 from ...domain import HigherIsBetterMetric, LowerIsBetterMetric
 
 
-class UnittestMetricMixin(SonarDashboardMetricMixin):
+class UnittestMetricMixin(SonarDashboardMetricMixin):  # pylint: disable=too-few-public-methods
     """ Mixin class for Sonar metrics about unit tests. """
 
     @classmethod
