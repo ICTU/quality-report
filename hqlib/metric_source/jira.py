92277f6fc9470d512084b5cb7c2c4ee66802e354
Frank Niessink <frank@niessink.com>
2017-03-26 16:33:54 +0200

    Remove some duplication. Add typing.

--- hqlib/metric_source/jira.py
+++ hqlib/metric_source/jira.py
@@ -15,6 +15,8 @@ limitations under the License.
 """
 
 
+from typing import Optional
+
 from . import url_opener
 from .. import utils, domain
 
@@ -32,7 +34,6 @@ class Jira(domain.MetricSource, url_opener.UrlOpener):
                  user_stories_without_performance_risk_query_id=None,
                  manual_test_cases_duration_field='customfield_11700',
                  user_story_points_field='customfield_10002'):
-        self.__url = url
         self.__open_bug_query_id = open_bug_query_id
         self.__open_security_bug_query_id = open_security_bug_query_id
         self.__open_static_security_analysis_bug_query_id = open_static_security_analysis_bug_query_id
@@ -43,7 +44,7 @@ class Jira(domain.MetricSource, url_opener.UrlOpener):
         self.__technical_debt_issues_query_id = technical_debt_issues_query_id
         self.__user_stories_without_security_risk_query_id = user_stories_without_security_risk_query_id
         self.__user_stories_without_performance_risk_query_id = user_stories_without_performance_risk_query_id
-        super().__init__(username=username, password=password)
+        super().__init__(url=url, username=username, password=password)
 
     def nr_open_bugs(self):
         """ Return the number of open bugs. """
@@ -87,31 +88,16 @@ class Jira(domain.MetricSource, url_opener.UrlOpener):
 
     def __query_total(self, query_id):
         """ Return the number of results of the specified query. """
-        if not query_id:
-            return -1
-        query_url = self.__get_query_url(query_id)
-        if not query_url:
-            return -1
-        try:
-            json_string = self.url_read(query_url)
-        except url_opener.UrlOpener.url_open_exceptions:
-            return -1
-        return int(utils.eval_json(json_string)['total'])
+        results = self.__get_query(query_id)
+        return int(results['total']) if results else -1
 
     def __query_sum(self, query_id, field):
         """ Return the sum of the fields as returned by the query. """
-        if not query_id:
-            return -1
-        query_url = self.__get_query_url(query_id)
-        if not query_url:
-            return -1
-        try:
-            json_string = self.url_read(query_url)
-        except url_opener.UrlOpener.url_open_exceptions:
+        results = self.__get_query(query_id)
+        if not results:
             return -1
         total = 0
-        issues = utils.eval_json(json_string)['issues']
-        for issue in issues:
+        for issue in results['issues']:
             try:
                 total += float(issue['fields'][field])
             except TypeError:
@@ -120,24 +106,30 @@ class Jira(domain.MetricSource, url_opener.UrlOpener):
 
     def __query_field_empty(self, query_id, field):
         """ Return the number of query results with field empty (null). """
-        if not query_id:
+        results = self.__get_query(query_id)
+        if not results:
             return -1
-        query_url = self.__get_query_url(query_id)
-        if not query_url:
-            return -1
-        json_string = utils.eval_json(self.url_read(query_url))
         total = 0
-        issues = json_string['issues']
-        for issue in issues:
+        for issue in results['issues']:
             try:
                 int(issue['fields'][field])
             except TypeError:
                 total += 1
         return total
 
-    def __get_query_url(self, query_id, search=True):
+    def __get_query(self, query_id):
+        """ Get the JSON from the query and evaluate it. """
+        query_url = self.__get_query_url(query_id)
+        try:
+            return utils.eval_json(self.url_read(query_url)) if query_url else None
+        except url_opener.UrlOpener.url_open_exceptions:
+            return None
+
+    def __get_query_url(self, query_id, search=True) -> Optional[str]:
         """ Get the query url based on the query id. """
-        url = self.__url + 'rest/api/2/filter/{qid}'.format(qid=query_id)
+        if not query_id:
+            return None
+        url = self.url() + 'rest/api/2/filter/{qid}'.format(qid=query_id)
         try:
             json_string = self.url_read(url)
         except url_opener.UrlOpener.url_open_exceptions:
@@ -145,38 +137,34 @@ class Jira(domain.MetricSource, url_opener.UrlOpener):
         url_type = 'searchUrl' if search else 'viewUrl'
         return utils.eval_json(json_string)[url_type]
 
-    def nr_open_bugs_url(self):
+    def nr_open_bugs_url(self) -> Optional[str]:
         """ Return the url for the nr of open bug reports query. """
         return self.__get_query_url(self.__open_bug_query_id, search=False)
 
-    def nr_open_security_bugs_url(self):
+    def nr_open_security_bugs_url(self) -> Optional[str]:
         """ Return the url for the nr of open security bug reports query. """
         return self.__get_query_url(self.__open_security_bug_query_id, search=False)
 
-    def nr_open_static_security_analysis_bugs_url(self):
+    def nr_open_static_security_analysis_bugs_url(self) -> Optional[str]:
         """ Return the url for the nr of open static security analysis bug reports query. """
         return self.__get_query_url(self.__open_static_security_analysis_bug_query_id, search=False)
 
-    def manual_test_cases_url(self):
+    def manual_test_cases_url(self) -> Optional[str]:
         """ Return the url for the manual test cases query. """
         return self.__get_query_url(self.__manual_test_cases_query_id, search=False)
 
-    def user_stories_ready_url(self):
+    def user_stories_ready_url(self) -> Optional[str]:
         """ Return the url for the ready user stories query. """
         return self.__get_query_url(self.__user_stories_ready_query_id, search=False)
 
-    def nr_technical_debt_issues_url(self):
+    def nr_technical_debt_issues_url(self) -> Optional[str]:
         """ Return the url for the technical debt issues query. """
         return self.__get_query_url(self.__technical_debt_issues_query_id, search=False)
 
-    def user_stories_without_security_risk_assessment_url(self):
+    def user_stories_without_security_risk_assessment_url(self) -> Optional[str]:
         """ Return the url for the user stories without security risk assessment query. """
         return self.__get_query_url(self.__user_stories_without_security_risk_query_id, search=False)
 
-    def user_stories_without_performance_risk_assessment_url(self):
+    def user_stories_without_performance_risk_assessment_url(self) -> Optional[str]:
         """ Return the url for the user stories without performance risk assessment query. """
         return self.__get_query_url(self.__user_stories_without_performance_risk_query_id, search=False)
-
-    def url(self):
-        """ Return the url of the Jira instance. """
-        return self.__url
