8532fd4227263b7efc84e7a931a628199b8bcb3e
Frank Niessink <frank@niessink.com>
2017-03-28 22:35:13 +0200

    Add type info.

--- hqlib/report/section.py
+++ hqlib/report/section.py
@@ -15,23 +15,27 @@ limitations under the License.
 """
 
 
+from typing import Sequence
+from ..domain import Metric, Product
+
+
 class SectionHeader(object):
     """ Header for a section, consisting of two-letter prefix, title and an optional subtitle. """
 
-    def __init__(self, id_prefix, title, subtitle=''):
+    def __init__(self, id_prefix: str, title: str, subtitle: str='') -> None:
         self.__id_prefix = id_prefix
         self.__title = title
         self.__subtitle = subtitle
 
-    def title(self):
+    def title(self) -> str:
         """ Return the title of the section. """
         return self.__title
 
-    def subtitle(self):
+    def subtitle(self) -> str:
         """ Return the subtitle of the section. """
         return self.__subtitle
 
-    def id_prefix(self):
+    def id_prefix(self) -> str:
         """ Return the id prefix of the section, a two letter string. """
         return self.__id_prefix
 
@@ -41,35 +45,35 @@ class SectionHeader(object):
 class Section(object):
     """ Section within a report. """
 
-    def __init__(self, header, metrics, product=None):
+    def __init__(self, header: SectionHeader, metrics: Sequence[Metric], product: Product=None) -> None:
         self.__header = header
         self.__metrics = metrics
         self.__product = product
         for index, each_metric in enumerate(self.__metrics):
             each_metric.set_id_string('{pref}-{nr}'.format(pref=self.__header.id_prefix(), nr=index + 1))
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.title()
 
-    def __getitem__(self, index):
+    def __getitem__(self, index: int) -> Metric:
         return self.__metrics[index]
 
-    def title(self):
+    def title(self) -> str:
         """ Return the title of this section. """
         return self.__header.title()
 
-    def subtitle(self):
+    def subtitle(self) -> str:
         """ Return the subtitle of this section. """
         return self.__header.subtitle()
 
-    def id_prefix(self):
+    def id_prefix(self) -> str:
         """ Return the id prefix of this section, a two letter string. """
         return self.__header.id_prefix()
 
-    def metrics(self):
+    def metrics(self) -> Sequence[Metric]:
         """ Return the metrics in this section. """
         return self.__metrics
 
-    def product(self):
+    def product(self) -> Product:
         """ Return the product this section is about. """
         return self.__product
