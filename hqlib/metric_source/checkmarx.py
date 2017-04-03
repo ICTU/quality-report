336be007cd9eb7e2f51e35508eed4f07bd0f216d
Dick Snel <dick.snel@ictu.nl>
2017-04-03 15:53:40 +0200

    Added Checkmarx metric

--- hqlib/metric_source/checkmarx.py
+++ hqlib/metric_source/checkmarx.py
@@ -52,7 +52,8 @@ class Checkmarx(domain.MetricSource):
     @staticmethod
     def __parse_alerts(json, risk_level):
         """ Parse the JSON to get the nr of alerts for the risk_level """
-
+        logging.warning("Parse: %s - %s", json, risk_level)
+        logging.warning("Parse2: %s", json["High"])
         return int(json[risk_level])
 
     def __fetch_report(self, project_name):
