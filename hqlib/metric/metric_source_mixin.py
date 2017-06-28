263880324e5a4dfc72e21c8b8e7216d004af4fa5
Frank Niessink <frank@niessink.com>
2017-06-28 21:43:54 +0200

    Bug fix: Allow for multiple SonarQube instances as metric source.

--- hqlib/metric/metric_source_mixin.py
+++ hqlib/metric/metric_source_mixin.py
@@ -36,25 +36,26 @@ class SonarMetric(domain.Metric):
 
     def _metric_source_urls(self) -> List[str]:
         """ Return the url to Sonar. """
-        return [self._metric_source.url()]
+        return [self._metric_source.url()] if self._metric_source else []
 
     def _sonar_id(self) -> str:
         """ Return the id of the subject in Sonar. """
-        return self._subject.metric_source_id(self._metric_source) or '' if self._subject else ''
+        return self._subject.metric_source_id(self._metric_source) or '' \
+            if (self._subject and self._metric_source) else ''
 
 
 class SonarDashboardMetric(SonarMetric):
     """ Class for metrics that use the Sonar dashboard. """
     def _metric_source_urls(self) -> List[str]:
         """ Return the url to the Sonar dashboard. """
-        return [self._metric_source.dashboard_url(self._sonar_id())]
+        return [self._metric_source.dashboard_url(self._sonar_id())] if self._metric_source else []
 
 
 class SonarViolationsMetric(SonarMetric):
     """ Class for metrics that use the Sonar violations. """
     def _metric_source_urls(self) -> List[str]:
         """ Return the url to the Sonar violations. """
-        return [self._metric_source.violations_url(self._sonar_id())]
+        return [self._metric_source.violations_url(self._sonar_id())] if self._metric_source else []
 
 
 class BirtTestDesignMetric(domain.Metric):
@@ -72,4 +73,4 @@ class BirtTestDesignMetric(domain.Metric):
     def _metric_source_urls(self) -> List[str]:
         """ Return the url for the What's Missing report instead of the Birt test design report since the
             What's Missing report allows users to click to the user stories and test cases in Jira. """
-        return [self._metric_source.whats_missing_url()]
+        return [self._metric_source.whats_missing_url()] if self._metric_source else []
