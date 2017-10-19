34036af5a08299ebdf14f59294fc026df271a2a4
Frank Niessink <frank@niessink.com>
2017-10-19 16:43:59 +0200

    Test in __init__.py isn't being discovered by the test runner, move to underscore-tests.py file.

--- backend/tests/unittests/version_tests.py
+++ backend/tests/unittests/version_tests.py
@@ -13,3 +13,13 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
+
+import unittest
+
+from hqlib import VERSION
+
+
+class VersionTest(unittest.TestCase):
+    def test_parts(self):
+        """ Test that the version number string consists of three integers. """
+        self.assertEqual([int, int, int], [type(int(part)) for part in VERSION.split('.')])
