c4332fd79feb9e1a4d9ee08796b25028bf28ffad
Frank Niessink <frank@niessink.com>
2017-12-18 20:17:02 +0100

    Less strict meta metric targets.

--- backend/hqlib/metric/meta_metrics.py
+++ backend/hqlib/metric/meta_metrics.py
@@ -50,8 +50,8 @@ class GreenMetaMetric(MetaMetric, HigherPercentageIsBetterMetric):
     norm_template = 'Minimaal {target}{unit} van de metrieken scoort groen (op of boven de norm). ' \
                     'Minder dan {low_target}{unit} is rood.'
     template = '{value}{unit} van de metrieken ({numerator} van de {denominator}) scoort boven de norm.'
-    target_value = 90
-    low_target_value = 80
+    target_value = 80
+    low_target_value = 70
 
 
 class RedMetaMetric(MetaMetric, LowerPercentageIsBetterMetric):
@@ -61,8 +61,8 @@ class RedMetaMetric(MetaMetric, LowerPercentageIsBetterMetric):
     norm_template = 'Maximaal {target}{unit} van de metrieken scoort rood (direct actie vereist). ' \
                     'Meer dan {low_target}{unit} is rood.'
     template = '{value}{unit} van de metrieken ({numerator} van de {denominator}) scoort rood.'
-    target_value = 2
-    low_target_value = 5
+    target_value = 10
+    low_target_value = 20
 
 
 class YellowMetaMetric(MetaMetric, LowerPercentageIsBetterMetric):
@@ -72,8 +72,8 @@ class YellowMetaMetric(MetaMetric, LowerPercentageIsBetterMetric):
     norm_template = 'Maximaal {target}{unit} van de metrieken scoort geel ' \
                     '(onder de norm maar niet direct actie vereist). Meer dan {low_target}{unit} is rood.'
     template = '{value}{unit} van de metrieken ({numerator} van de {denominator}) scoort geel.'
-    target_value = 5
-    low_target_value = 10
+    target_value = 20
+    low_target_value = 30
 
 
 class GreyMetaMetric(MetaMetric, LowerPercentageIsBetterMetric):
@@ -83,8 +83,8 @@ class GreyMetaMetric(MetaMetric, LowerPercentageIsBetterMetric):
     norm_template = 'Maximaal {target}{unit} van de metrieken scoort grijs (geaccepteerde technische schuld). ' \
                     'Meer dan {low_target}{unit} is rood.'
     template = '{value}% van de metrieken ({numerator} van de {denominator}) scoort grijs.'
-    target_value = 2
-    low_target_value = 5
+    target_value = 20
+    low_target_value = 30
 
 
 class MissingMetaMetric(MetaMetric, LowerPercentageIsBetterMetric):
@@ -94,5 +94,5 @@ class MissingMetaMetric(MetaMetric, LowerPercentageIsBetterMetric):
     norm_template = 'Maximaal {target}{unit} van de metrieken kan niet gemeten worden. ' \
                     'Meer dan {low_target}{unit} is rood.'
     template = '{value}{unit} van de metrieken ({numerator} van de {denominator}) kan niet gemeten worden.'
-    target_value = 0
-    low_target_value = 5
+    target_value = 20
+    low_target_value = 30
