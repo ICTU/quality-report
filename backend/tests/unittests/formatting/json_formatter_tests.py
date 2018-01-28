1778319a9f350b86aa45f2b1b66e21d8c579c17e
Frank Niessink <frank@niessink.com>
2018-01-28 18:18:46 +0100

    Roll back tag feature.

--- backend/tests/unittests/formatting/json_formatter_tests.py
+++ backend/tests/unittests/formatting/json_formatter_tests.py
@@ -85,7 +85,7 @@ class MetricsFormatterTest(unittest.TestCase):
 "status_format": "<img src='img/sad.png' alt=':-(' width='48' height='48' \
 title='Direct actie vereist: norm niet gehaald (sinds 1 januari 2012)' border='0' />", \
 "status_start_date": [2012, 1, 1, 12, 0, 0], "measurement": "report [label: <a href='http://url' \
-target='_blank'>anchor</a>]", "norm": "norm", "comment": "Comment with \\\\backslash", "tags": [], \
+target='_blank'>anchor</a>]", "norm": "norm", "comment": "Comment with \\\\backslash", \
 "extra_info": {{"headers": \
 {{"col1": "C1", "col2": "C2"}}, "title": "Fake title", "data": [{{"col1": "yes", "col2": {{"href": "this", "text": \
 "that"}}}}]}}}}, {{"id_value": "id_string-01", "id_format": "id_string-1", "name": "Metric Name", "section": \
@@ -93,7 +93,7 @@ target='_blank'>anchor</a>]", "norm": "norm", "comment": "Comment with \\\\backs
 "status_value": "0", "status_format": "<img src='img/sad.png' alt=':-(' width='48' height='48' \
 title='Direct actie vereist: norm niet gehaald (sinds 1 januari 2012)' border='0' />", \
 "status_start_date": [2012, 1, 1, 12, 0, 0], "measurement": "report [label: <a href='http://url' \
-target='_blank'>anchor</a>]", "norm": "norm", "comment": "Comment with \\\\backslash", "tags": [], \
+target='_blank'>anchor</a>]", "norm": "norm", "comment": "Comment with \\\\backslash", \
 "extra_info": {{"headers": {{"col1": "C1", "col2": "C2"}}, "title": "Fake title", "data": [{{"col1": "yes", "col2": \
 {{"href": "this", "text": "that"}}}}]}}}}]}}\n'''.format(VERSION), self.__formatter.process(fake_report.Report()))
 
